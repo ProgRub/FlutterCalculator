c4a3304045cc986b9de2e717d452746327331b75
Ruben Rodrigues <ruben2000@gmail.com>
2022-03-03 22:31:05 +0000

    Numbers displayed, need to add space every 3

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -5,38 +5,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MaterialApp(home: new Calculator()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Calculator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _CalculatorState();
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Calculator',
+  //     home:
+  //       // body: GridView.count(
+  //       //     crossAxisCount: 3,
+  //       //     children: List.generate(10, (index) {
+  //       //       return TextButton(
+  //       //           onPressed: () =>
+  //       //               {registerNumber(index==9?0:(3 - index ~/ 3) * 3 - 2 + index % 3)},
+  //       //           child: Text(
+  //       //               (index==9?0:(3 - index ~/ 3) * 3 - 2 + index % 3).toString()));
+  //       //     }))
+
+  //       // new Center(
+  //       //   child: TextButton(onPressed: () => {registerNumber(1)}, child: Text('1')),
+  //       // ),
+  //     ),
+  //   );
+  // }
+
+}
+
+class _CalculatorState extends State<Calculator> {
+  late TextEditingController displayController;
+  @override
+  void initState() {displayController=TextEditingController();
+  displayController.text="0";}
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calculator',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Calculator'),
+    return new Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Calculator'),
+        ),
+        body: Column(children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(controller: displayController,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+                readOnly: true),
           ),
-          body: GridView.count(
-              crossAxisCount: 3,
-              children: List.generate(10, (index) {
-                return TextButton(
-                    onPressed: () =>
-                        {registerNumber(index==9?0:(3 - index ~/ 3) * 3 - 2 + index % 3)},
-                    child: Text(
-                        (index==9?0:(3 - index ~/ 3) * 3 - 2 + index % 3).toString()));
-              }))
-
-          // new Center(
-          //   child: TextButton(onPressed: () => {registerNumber(1)}, child: Text('1')),
-          // ),
-          ),
-    );
+          Row(children: [
+            TextButton(onPressed: () => {registerNumber(7)}, child: Text('7')),
+            TextButton(onPressed: () => {registerNumber(8)}, child: Text('8')),
+            TextButton(onPressed: () => {registerNumber(9)}, child: Text('9'))
+          ]),
+          Row(children: [
+            TextButton(onPressed: () => {registerNumber(4)}, child: Text('4')),
+            TextButton(onPressed: () => {registerNumber(5)}, child: Text('5')),
+            TextButton(onPressed: () => {registerNumber(6)}, child: Text('6'))
+          ]),
+          Row(children: [
+            TextButton(onPressed: () => {registerNumber(1)}, child: Text('1')),
+            TextButton(onPressed: () => {registerNumber(2)}, child: Text('2')),
+            TextButton(onPressed: () => {registerNumber(3)}, child: Text('3'))
+          ]),
+          Row(children: [
+            TextButton(
+                onPressed: () => {registerNumber(15)}, child: Text("AC")),
+            TextButton(onPressed: () => {registerNumber(0)}, child: Text('0'))
+          ])
+        ]));
   }
 
   void registerNumber(int number) {
-    print(number);
+    var text=displayController.text;
+    var displayNumber=int.parse(text);
+    displayController.text=(displayNumber*10+number).toString();
   }
 }
